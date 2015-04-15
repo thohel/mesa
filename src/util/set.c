43bc8972c64e8ffa9f4496ccb2be5f6e052d1fef
Thomas Helland <thomashelland90@gmail.com>
2015-04-15 23:08:19 +0200

    util: Use set_foreach instead of rolling our own
    This follows the same pattern as in the hash_table.
    
    Reviewed-by: Jason Ekstrand <jason.ekstrand at intel.com>

--- src/util/set.c
+++ src/util/set.c
@@ -196,12 +196,8 @@ set_rehash(struct set *ht, uint32_t new_size_iteration)
    ht->entries = 0;
    ht->deleted_entries = 0;
 
-   for (entry = old_ht.table;
-        entry != old_ht.table + old_ht.size;
-        entry++) {
-      if (entry_is_present(entry)) {
-         set_add(ht, entry->hash, entry->key);
-      }
+   set_foreach(&old_ht, entry) {
+      set_add(ht, entry->hash, entry->key);
    }
 
    ralloc_free(old_ht.table);
